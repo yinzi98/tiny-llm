e9408e3de14111f913d2586d0da80a9fb1500748
zhuohuayin <huayin.zhuo@foxmail.com>
2026-03-15 16:52:45 +0800

    d5 t2

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -1,12 +1,14 @@
 import mlx.core as mx
-
+from .basics import linear
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        return self.weight[x]
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        return linear(x, self.weight)
